2438d33b296a30613c8313a604365a9917ebe22b
angelajburden <angela.burden@yale.edu>
2018-01-23 21:47:29 -0500

    updated to be more efficient

--- NN_HSC_params.py
+++ NN_HSC_params.py
@@ -18,14 +18,14 @@ HLs = [1]
 HLn=[10]
 alpha = [0.1]
 '''
-HLs = [1,2,3,4]
-HLn=[10,20,50,100]
-alpha = [0.1,0.01, 0.001,0.0001]
+HLs = [4]
+HLn=[50]
+alpha = [0.001]
 svers = ['lbfgs','sgd','adam']
 
 num_labels = 1;
 
-importlib.reload(rdHSC)
+reload(rdHSC)
 mag_flag =2
 # Load Training Data
 TA = rdHSC.sort_data(mag_flag)
@@ -44,27 +44,27 @@ av=[]
 HL=[]
 HLN=[]
 solver = svers[2]
+print('\n Now test it\n')                                                                                    
+TD = rdHSC.sort_test_data(mag_flag)
+
+TDall = np.array([TD['g_r'],TD['r_z'],TD['g_z'],TD['g_W1'],TD['r_W1'],
+      TD['z_W1'],TD['g_W2'],TD['r_W2'],TD['z_W2'],TD['W1_W2'],TD['r'], TD['y']])
+TestData = TDall.transpose() 
+print(TestData.shape)      
+X_test = TestData[:,:-1]  
+y_test = TestData[:,-1]
 
 for alphaval in alpha:
     for HLsize in HLs:
         for HLnodes in HLn: 
-            clf = MLPClassifier(activation='logistic',solver=solver, alpha=alphaval, hidden_layer_sizes=(HLsize, HLnodes), verbose=True, random_state=1)
-            clf.fit(X, y)
-
-            print('\n Now test it\n')                                                                                    
-            TD = rdHSC.sort_test_data(mag_flag)
-
-            TDall = np.array([TD['g_r'],TD['r_z'],TD['g_z'],TD['g_W1'],TD['r_W1'],
-                  TD['z_W1'],TD['g_W2'],TD['r_W2'],TD['z_W2'],TD['W1_W2'],TD['r'], TD['y']])
-            TestData = TDall.transpose() 
-            print(TestData.shape)      
-            X_test = TestData[:,:-1]  
-            y_test = TestData[:,-1]
-            b = clf.predict(X_test)
+            clf = MLPClassifier(activation='logistic',solver = solver, alpha=alphaval, hidden_layer_sizes=(HLsize, HLnodes), verbose=True, random_state=1)
+            clf.fit(X, y.ravel())
+            b2 = clf.predict(X_test)
+            
             yones  = y_test[y_test==1]
             yzeros  = y_test[y_test==0]
-            NNones = b[y_test==1]
-            NNzeros = b[y_test==0]
+            NNones = b2[y_test==1]
+            NNzeros = b2[y_test==0]
 
             cTP = yones==NNones
             cTN = yzeros=NNzeros
@@ -81,5 +81,26 @@ for alphaval in alpha:
             HL.append(HLsize)
             HLN.append(HLnodes)
 
-fileout = 'params_' + solver + '.txt'
+fileout = 'params_NN_test'  + '.txt'
 np.savetxt(fileout, np.c_[av,HL,HLN,TP,TN,FP,FN,prec,recall],fmt='%1.4f')
+from sklearn import metrics
+b2_prob = clf.predict_proba(X_test)
+fpr, tpr, _ = metrics.roc_curve(y_test, b2_prob[:,1])
+auc = metrics.auc(fpr,tpr)
+import matplotlib.pyplot as plt
+plt.plot(fpr,tpr)
+plt.fill_between(fpr, 0, tpr,facecolor='powderblue')
+plt.xlabel('fpr')
+plt.ylabel('tpr')
+plt.title('ROC Curve for NN w/AUC =0.84')
+plt.savefig('ROC_NN.pdf')
+
+
+
+
+
+
+
+
+
+
